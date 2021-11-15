b3ebd59fd7f358363f49efcf45785bb5e83bc447
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-11-15 08:51:15 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Usbong Tugon System
+#
+# C/C++ Computer Language and Simple Directmedia Layer (SDL) + SDL Image
+#
+# Auto-install Application (with required libraries)
+#
+# @company: USBONG
+# @author: SYSON, MICHAEL B.
+# @date created: 20211115
+# @last updated: 20211115
+# @website address: http://www.usbong.ph
+#
+# Additional Notes:
+# 1) If asked by Computer what to do with the executable file, select: "Execute in Terminal"
+# 
+
+sudo apt-get update
+sudo apt-get install libsdl2-dev
+sudo apt-get install libsdl2-image-dev
