04d8d9e080a449c36acfaa103fdfc5b77a24f685
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-11-15 13:56:07 +0800

    Add files via upload

---
+++ execute.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usbong Tugon System: Execution
+#
+# C/C++ Computer Language and Simple Directmedia Layer (SDL) + SDL Image
+#
+# Compile and Link Multiple Source Files
+#	Execute Output Application
+#
+# @company: USBONG
+# @author: SYSON, MICHAEL B.
+# @date created: 20211115
+# @last updated: 20211115
+# @website address: http://www.usbong.ph
+
+#note: sequence
+./make.sh
+
+cd ./output
+./UsbongTugonLinux
